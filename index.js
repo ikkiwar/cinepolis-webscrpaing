e1d21632317c1cddcc05f839b7805cca9e46442f
ikkiwar <ikkiofbodom@gmail.com>
2021-02-24 23:20:12 -0600

    It already brings the url of the schedules

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+  await page.setViewport({ width: 1600, height: 900 });
   await page.goto("https://www.cinepolis.com.sv/");
   await page.click(".btn.btnEnviar.btnVerCartelera");
-  await page.screenshot({ path: "example.png" });
+  await page.goto(
+    "https://www.cinepolis.com.sv/cartelera/san-salvador-el-salvador",
+    [60000, { waitUntil: "domcontentloaded" }]
+  );
+  let data = await page.evaluate(() => {
+    const url = [...document.querySelectorAll(".btn.btnhorario.ng-scope")].map(
+      (info) => info.innerHTML
+    );
 
+    return url;
+  });
+  /*  await page.screenshot({ path: "example.png" });  preloadCartelera*/
+  console.log(data);
   await browser.close();
 })();
