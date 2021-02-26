de3cf5f18f69d4b4277bc572582554f029b94f8b
degf21 <31782406+degf21@users.noreply.github.com>
2021-02-26 10:01:03 -0600

    Update selenium_cine.py

--- selenium_cine.py
+++ selenium_cine.py
@@ -19,10 +19,22 @@ driver.get('https://www.cinepolis.com.sv/')
 
 WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'input.btn.btnEnviar.btnVerCartelera'))).click()
 
-lista = driver.find_elements_by_class_name('btn.btnhorario.ng-scope')
+lista_ss = driver.find_elements_by_class_name('btn.btnhorario.ng-scope')
 
-for href in lista:
-    link = href.find_element_by_tag_name('a').get_attribute('href')
-    hora = href.find_element_by_tag_name('a').text
-    print(link)
-    print(hora)
+for href in lista_ss:
+    link_ss = href.find_element_by_tag_name('a').get_attribute('href')
+    hora_ss = href.find_element_by_tag_name('a').text
+    print(link_ss)
+    print(hora_ss)
+
+WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'option:nth-child(3)'))).click()
+WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'input.btn.btnEnviar.btnVerCartelera'))).click()
+
+
+lista_sa = driver.find_elements_by_class_name('btn.btnhorario.ng-scope')
+
+for href in lista_sa:
+    link_sa = href.find_element_by_tag_name('a').get_attribute('href')
+    hora_sa = href.find_element_by_tag_name('a').text
+    print(link_sa)
+    print(hora_sa)
