f6d56778050d795ebcf2201272d0f0bc8c5690f0
degf21 <31782406+degf21@users.noreply.github.com>
2021-03-23 15:50:10 -0600

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
 import pymongo
 from bson.json_util import dumps
 import requests
 
 app = Flask(__name__)
-Data = []
+Data_Galerias = []
+Data_Multi = []
+Data_VIP = []
+Data_SA = []
 
-#Conexion a la base de datos
 MONGO_HOST = "localhost"
 MONGO_PUERTO = "27017"
 MONGO_TIEMPO_FUERA = 1000
@@ -21,20 +23,49 @@ coleccion = baseDatos[MONGO_COLECCION]
 
 
 def getFecha():
-    data = coleccion.find({"fecha": "19-marzo-2021"})
-    for r in data:
-        Data.append(r)
-    json_data = dumps(Data)
+    data_galerias = coleccion.find({"cine": "Galerias", "fecha": "19-marzo-2021"})
+    data_multi = coleccion.find({"cine": "Multiplaza", "fecha": "19-marzo-2021"})
+    data_VIP = coleccion.find({"cine": "VIP Galerias", "fecha": "19-marzo-2021"})
+    data_sa = coleccion.find({"cine": "Santa Ana", "fecha": "19-marzo-2021"})
+
+    for galerias in data_galerias:
+        Data_Galerias.append(galerias)
+
+    for multi in data_multi:
+        Data_Multi.append(multi)
+
+    for vip in data_VIP:
+        Data_VIP.append(vip)
+
+    for sa in data_sa:
+        Data_SA.append(sa)
+
+    json_data = dumps({"Galerias": Data_Galerias, "Multiplaza": Data_Multi, "VIP Galerias": Data_VIP, "Santa Ana": Data_SA})
     cliente.close()
     return json_data
 
 
 def getAll():
-    data = coleccion.find()
-    for r in data:
-        Data.append(r)
-    json_data = dumps(Data)
+    data_galerias = coleccion.find({"cine": "Galerias"})
+    data_multi = coleccion.find({"cine": "Multiplaza"})
+    data_VIP = coleccion.find({"cine": "VIP Galerias"})
+    data_sa = coleccion.find({"cine": "Santa Ana"})
+
+    for galerias in data_galerias:
+        Data_Galerias.append(galerias)
+
+    for multi in data_multi:
+        Data_Multi.append(multi)
+
+    for vip in data_VIP:
+        Data_VIP.append(vip)
+
+    for sa in data_sa:
+        Data_SA.append(sa)
+
+    json_data = dumps({"Galerias": Data_Galerias, "Multiplaza": Data_Multi, "VIP Galerias": Data_VIP, "Santa Ana": Data_SA})
     cliente.close()
+
     return json_data
 
 
